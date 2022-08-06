63e8ace7a0fec2133bb23afa1a90c9f1fad8e86e
Jackie <jye0325@gmail.com>
2022-08-06 09:12:15 -0400

    commit tree e6a83fce0fb2a5c44a59946be0236ccc0550041a

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,3 +1,19 @@
 class Company < ActiveRecord::Base
+    has_many :freebies
+    has_many :devs, through: :freebies
+
+    # #give_freebie(dev, item_name, value)
+    # takes a dev (an instance of the Dev class), an item_name (string), 
+    # and a value as arguments, and creates a new Freebie instance associated 
+    # with this company and the given dev 
+    def give_freebie(dev, item_name, value)
+        Freebie.create(item_name: item_name, value: value, dev: dev, company: self)
+    end
+
+    # .oldest_company
+    # returns the Company instance with the earliest founding year
+    def self.oldest_company
+        self.order(founding_year: :asc).limit(1)
+    end
 
 end
--- app/models/freebie.rb
+++ app/models/freebie.rb
@@ -1,3 +1,11 @@
 class Freebie < ActiveRecord::Base
-
+    belongs_to :company
+    belongs_to :dev
+    # #print_details
+    # should return a string formatted as follows: 
+    # {insert dev's name} owns a {insert freebie's item_name} 
+    # from {insert company's name}
+    def print_details
+        "#{dev.name} owns a #{item_name} from #{company.name}"
+    end
 end
